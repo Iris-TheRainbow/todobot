b10f7b6cb23e34853cdcd9a16e32308627594963
iris <snazzsincalir@gmail.com>
2024-07-10 02:58:01 -0500

    sorta got stuff working

--- __main__.py
+++ __main__.py
@@ -1,1 +1,32 @@
 import discord
+import os
+import json
+import api
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    if not os.path.exists('knownUsers'):
+        open('knownUsers', 'w').close()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('todo'):
+        user = str(message.author)
+        with open('knownUsers', 'r') as knownUsers:
+            if not user in knownUsers.read().splitlines():
+                open('knownUsers', 'a').write(user + '\n')
+                await message.channel.send('First time user!')
+            knownUsers.close()
+
+        if
+
+client.run(api.getKey())
