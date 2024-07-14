60fe39e8ef4ebcc9ad337beb2319ed78177404e9
iris <snazzsinclair@gmail.com>
2024-07-14 06:04:52 -0500

    the basic functions work now!

---
+++ User.py
@@ -0,0 +1,37 @@
+class User():
+    def __init__(self, todopath, name):
+        self.path = todopath
+        with open(self.path, 'r') as f:
+            self.todo = f.read().splitlines()
+            f.close()
+        for i in range(len(self.todo)):
+            if not self.todo[i].endswith('\n'):
+                self.todo[i] += '\n'
+        self.name = name
+
+    def read(self):
+        return self.todo
+
+    def add(self, message, place = 0):
+        message += '\n'
+        if place != 0:
+            self.todo.insert(place, message)
+        else:
+            self.todo.append(message)
+        self.update()
+
+    def move(self, initial, position):
+        data = self.todo[initial]
+        self.todo.pop(initial)
+        self.todo.insert(position, data)
+        self.update()
+
+    def remove(self, position):
+        self.todo.pop(position)
+        self.update()
+
+    def update(self):
+        print(self.todo)
+        with open(self.path, 'w') as f:
+            f.writelines(self.todo)
+            f.close()
--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import discord
 import os
 import json
 import api
+import User
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,16 +23,44 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith('todo'):
-        user = str(message.author)
+    if message.content.lower().split()[0]:
+        name = str(message.author)
         with open('knownUsers', 'r') as knownUsers:
-            if not user in knownUsers.read().splitlines():
-                open('knownUsers', 'a').write(user + '\n')
-                await message.channel.send('First time user!')
+            if not name in knownUsers.read().splitlines():
+                open('knownUsers', 'a').write(name + '\n')
+                await message.channel.send('Hello new user! Add something to your todo with `todo add new todo item`')
             knownUsers.close()
 
-        todo = 'users/' + user
+        todo = 'users/' + name
         if not os.path.exists(todo):
             open(todo, 'w').close()
+        user = User.User(todo, name)
+        content = message.content.split()
+        if len(content) <= 1: return
+        if content[1] == 'list':
+            msg = 'Your todo is:\n'
+            for i in range(len(user.read())):
+                msg += str(i + 1) + '. ' + str(user.read()[i])
+            await message.channel.send(msg)
+
+        if content[1] == 'add':
+            if len(content) >= 3:
+                if content[-1].isdigit():
+                    todo = ''
+                    for i in range(2, len(content ) - 1):
+                        print(content[i])
+                        todo += content[i] + ' '
+                    user.add(todo, int(content[-1]) - 1)
+                    return
+            todo = ''
+            for i in range(2, len(content)):
+                print(content[i])
+                todo += content[i] + ' '
+            user.add(todo)
+        if content[1] == 'move':
+            user.move(int(content[2]) - 1, int(content[3]) - 1)
+
+        if content[1] == 'remove':
+            user.remove(int(content[2]) - 1)
 
 client.run(api.getKey())
